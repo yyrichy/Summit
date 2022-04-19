d59f6d528c6ce265fafcf759b4ff7cbbac9b0982
VapoR <ryvapor@gmail.com>
2022-04-19 17:18:37 -0400

    add course components

---
+++ components/Course.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { StyleSheet, View, Text } from "react-native"
+
+function CourseComponent(props) {
+  return (
+    <View style={[styles.container, props.style]}>
+      <Text style={styles.name}>{props.name}</Text>
+      <Text style={styles.mark}>{props.mark}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "rgb(221,221,221)",
+    borderRadius: 15,
+    height: 50,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginLeft: 7,
+    marginRight: 7,
+    marginTop: 7
+  },
+  name: {
+    color: "#121212",
+    fontSize: 18,
+    marginLeft: 20
+  },
+  mark: {
+    color: "#121212",
+    fontSize: 35,
+    alignSelf: "flex-end",
+    marginRight: 20
+  }
+})
+
+export default CourseComponent
--- screens/Grades.tsx
+++ screens/Grades.tsx
@@ -1,23 +1,29 @@
-import React, { useContext, useEffect, useRef } from "react"
+import React, { useContext, useRef } from "react"
 import AppContext from "../components/AppContext"
-import { View, Text, Alert } from "react-native"
+import { View } from "react-native"
 import { useState } from "react"
-import { Gradebook } from "studentvue/StudentVue/Client/Interfaces/Gradebook"
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler"
+import CourseComponent from "../components/Course"
 
 const Grades = () => {
   const componentMounted = useRef(true)
   const context = useContext(AppContext)
-	const gradebook = context.gradebook;
+  const gradebook = context.gradebook
+  const [courses, setCourses] = useState( 
+    gradebook.courses
+  )
 
   return (
     <View>
-      <Text>
-        {gradebook == undefined
-          ? ""
-          : gradebook.courses.map(
-              (c) => `${c.title} - ${c.marks[0].calculatedScore.raw}\n`
-            )}
-      </Text>
+      <FlatList
+        data={courses}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <CourseComponent name={item.title} mark={item.marks[0].calculatedScore.string}></CourseComponent>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.title}
+      />
     </View>
   )
 }
