c3a3f2d5dc8842ea75cbb54894107e4100905630
VapoR <ryvapor@gmail.com>
2022-05-12 19:06:51 -0400

    fetch student info
    waiting on https://github.com/EGGaming/studentvue.js/issues/1

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useContext, useState } from 'react'
+import { ActivityIndicator, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Colors } from '../colors/Colors'
+import AppContext from '../contexts/AppContext'
 
 const Profile = () => {
+  const { client } = useContext(AppContext)
+  const [studentInfo, setStudentInfo] = useState(undefined)
+
+  const fetchStudentInfo = async () => {
+    setStudentInfo(await client.studentInfo())
+  }
+  if (!studentInfo) fetchStudentInfo()
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Text>This is the profile screen</Text>
+      {!studentInfo ? (
+        <ActivityIndicator
+          color={Colors.secondary}
+          animating={true}
+          size="large"
+          style={{
+            alignSelf: 'center',
+            flex: 1,
+            justifyContent: 'center'
+          }}
+        />
+      ) : (
+        <Text>Student info defined and shown here</Text>
+      )}
     </SafeAreaView>
   )
 }
