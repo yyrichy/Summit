32149ccaccd37f3aa8cb87bce6db162ef7171537
VapoR <ryvapor@gmail.com>
2022-11-08 03:18:30 -0500

    new profile screen ui

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -5,7 +5,8 @@ import {
   StyleSheet,
   View,
   Text,
-  BackHandler
+  BackHandler,
+  SafeAreaView
 } from 'react-native'
 import { SchoolInfo, StudentInfo } from 'studentvue'
 import { Colors } from '../colors/Colors'
@@ -19,7 +20,6 @@ import {
 import { suffix } from '../gradebook/GradeUtil'
 import useAsyncEffect from 'use-async-effect'
 import { ScrollView } from 'react-native-gesture-handler'
-import Constants from 'expo-constants'
 
 const Profile = ({ navigation }) => {
   const { client } = useContext(AppContext)
@@ -43,9 +43,9 @@ const Profile = ({ navigation }) => {
     return () => backHandler.remove()
   }, [])
 
-  return (
-    <View style={{ flex: 1 }}>
-      {!studentInfo || !schoolInfo ? (
+  if (!studentInfo || !schoolInfo) {
+    return (
+      <View style={{ flex: 1 }}>
         <ActivityIndicator
           color={Colors.secondary}
           animating={true}
@@ -56,138 +56,156 @@ const Profile = ({ navigation }) => {
             justifyContent: 'center'
           }}
         />
-      ) : (
-        <View style={{ flex: 1 }}>
-          <View style={styles.header}></View>
-          <Image
-            style={styles.avatar}
-            source={{
-              uri: `data:image/png;base64,${studentInfo.photo}`
-            }}
-          />
+      </View>
+    )
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.avatar_info_container}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: `data:image/png;base64,${studentInfo.photo}`
+          }}
+        />
+        <View style={styles.info_container}>
           <Text style={styles.name}>{studentInfo.student.name}</Text>
-          <View style={styles.description_container}>
-            <Text style={styles.description_part_text}>
-              {studentInfo.grade + suffix(parseInt(studentInfo.grade))}
-            </Text>
-            <Text style={styles.description_part_text}>
-              {studentInfo.birthDate.toLocaleDateString()}
+          <View style={styles.details_container}>
+            <View style={styles.detaiL_container}>
+              <Text style={styles.detail_value}>
+                {studentInfo.grade + suffix(parseInt(studentInfo.grade))}
+              </Text>
+              <Text style={styles.detail_name}>Grade</Text>
+            </View>
+            <View style={styles.detaiL_container}>
+              <Text style={styles.detail_value}>
+                {studentInfo.birthDate.toLocaleDateString()}
+              </Text>
+              <Text style={styles.detail_name}>Birthdate</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      <ScrollView style={styles.property_view}>
+        {studentInfo.id && (
+          <View style={styles.property_container}>
+            <AntDesign name="idcard" size={22} color={Colors.black} />
+            <Text style={styles.property_text}>
+              {studentInfo.id ? studentInfo.id : ''}
             </Text>
           </View>
-          <ScrollView style={styles.property_view}>
-            {studentInfo.id && (
-              <View style={styles.property_container}>
-                <AntDesign name="idcard" size={22} color={Colors.black} />
-                <Text style={styles.property_text}>
-                  {studentInfo.id ? studentInfo.id : ''}
-                </Text>
-              </View>
-            )}
-            {studentInfo.phone && (
-              <View style={styles.property_container}>
-                <Feather name="phone" size={22} color={Colors.black} />
-                <Text style={styles.property_text}>{studentInfo.phone}</Text>
-              </View>
-            )}
-            {studentInfo.address && (
-              <View style={styles.property_container}>
-                <Feather name="home" size={22} color={Colors.black} />
-                <Text style={styles.property_text}>{studentInfo.address}</Text>
-              </View>
-            )}
-            {studentInfo.email && (
-              <View style={styles.property_container}>
-                <Feather name="mail" size={22} color={Colors.black} />
-                <Text style={styles.property_text}>{studentInfo.email}</Text>
-              </View>
-            )}
-            {studentInfo.currentSchool && (
-              <View style={styles.property_container}>
-                <FontAwesome name="building-o" size={22} color={Colors.black} />
-                <Text style={styles.property_text}>
-                  {studentInfo.currentSchool}
-                </Text>
-              </View>
-            )}
-            {schoolInfo.school.address && (
-              <View style={styles.property_container}>
-                <Feather name="map-pin" size={22} color={Colors.black} />
-                <Text style={styles.property_text}>
-                  {schoolInfo.school.address}
-                </Text>
-              </View>
-            )}
-            {studentInfo.homeRoom && (
-              <View style={styles.property_container}>
-                <FontAwesome
-                  name="pencil-square-o"
-                  size={22}
-                  color={Colors.black}
-                />
-                <Text style={styles.property_text}>
-                  Homeroom: {studentInfo.homeRoom}
-                </Text>
-              </View>
-            )}
-            {studentInfo.counselor && (
-              <View style={styles.property_container}>
-                <Feather name="user" size={22} color={Colors.black} />
-                <Text style={styles.property_text}>
-                  Counselor: {studentInfo.counselor.email}
-                </Text>
-              </View>
-            )}
-            {schoolInfo.school.principal && (
-              <View style={styles.property_container}>
-                <MaterialCommunityIcons
-                  name="crown-outline"
-                  size={22}
-                  color={Colors.black}
-                />
-                <Text style={styles.property_text}>
-                  Principal: {schoolInfo.school.principal.email}
-                </Text>
-              </View>
-            )}
-          </ScrollView>
-        </View>
-      )}
-    </View>
+        )}
+        {studentInfo.phone && (
+          <View style={styles.property_container}>
+            <Feather name="phone" size={22} color={Colors.black} />
+            <Text style={styles.property_text}>{studentInfo.phone}</Text>
+          </View>
+        )}
+        {studentInfo.address && (
+          <View style={styles.property_container}>
+            <Feather name="home" size={22} color={Colors.black} />
+            <Text style={styles.property_text}>{studentInfo.address}</Text>
+          </View>
+        )}
+        {studentInfo.email && (
+          <View style={styles.property_container}>
+            <Feather name="mail" size={22} color={Colors.black} />
+            <Text style={styles.property_text}>{studentInfo.email}</Text>
+          </View>
+        )}
+        {studentInfo.currentSchool && (
+          <View style={styles.property_container}>
+            <FontAwesome name="building-o" size={22} color={Colors.black} />
+            <Text style={styles.property_text}>
+              {studentInfo.currentSchool}
+            </Text>
+          </View>
+        )}
+        {schoolInfo.school.address && (
+          <View style={styles.property_container}>
+            <Feather name="map-pin" size={22} color={Colors.black} />
+            <Text style={styles.property_text}>
+              {schoolInfo.school.address}
+            </Text>
+          </View>
+        )}
+        {studentInfo.homeRoom && (
+          <View style={styles.property_container}>
+            <FontAwesome
+              name="pencil-square-o"
+              size={22}
+              color={Colors.black}
+            />
+            <Text style={styles.property_text}>
+              Homeroom: {studentInfo.homeRoom}
+            </Text>
+          </View>
+        )}
+        {studentInfo.counselor && (
+          <View style={styles.property_container}>
+            <Feather name="user" size={22} color={Colors.black} />
+            <Text style={styles.property_text}>
+              Counselor: {studentInfo.counselor.email}
+            </Text>
+          </View>
+        )}
+        {schoolInfo.school.principal && (
+          <View style={styles.property_container}>
+            <MaterialCommunityIcons
+              name="crown-outline"
+              size={22}
+              color={Colors.black}
+            />
+            <Text style={styles.property_text}>
+              Principal: {schoolInfo.school.principal.email}
+            </Text>
+          </View>
+        )}
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  header: {
-    height: Constants.statusBarHeight + 90,
-    backgroundColor: Colors.primary
-  },
   name: {
-    fontSize: 32,
-    fontFamily: 'Montserrat_800ExtraBold',
-    alignSelf: 'center',
-    margin: 5
+    fontSize: 30,
+    fontFamily: 'Montserrat_700Bold'
   },
   avatar: {
-    width: 125,
-    height: 125,
-    borderRadius: 125 / 2,
+    width: 120,
+    height: 120,
+    borderRadius: 120 / 2,
     borderWidth: 1,
-    borderColor: Colors.black,
-    alignSelf: 'center',
-    marginTop: -125 / 2
+    borderColor: Colors.black
   },
-  description_container: {
+  avatar_info_container: {
+    margin: 25,
     flexDirection: 'row',
-    justifyContent: 'center'
+    justifyContent: 'flex-start'
   },
-  description_part_text: {
-    marginHorizontal: 10,
-    marginBottom: 10,
-    fontSize: 24,
-    fontFamily: 'Montserrat_600SemiBold'
+  info_container: {
+    justifyContent: 'center',
+    marginLeft: 20
+  },
+  details_container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 10
+  },
+  detaiL_container: {
+    alignItems: 'center'
+  },
+  detail_value: {
+    fontSize: 20,
+    fontFamily: 'Montserrat_500Medium'
+  },
+  detail_name: {
+    fontFamily: 'Montserrat_300Light',
+    fontSize: 14
   },
   property_view: {
-    borderRadius: 20,
+    borderRadius: 15,
     borderWidth: 1,
     borderColor: Colors.secondary,
     backgroundColor: Colors.off_white,
