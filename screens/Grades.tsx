cc60beb434053334f851e14527ce61043cd3fbd9
VapoR <ryvapor@gmail.com>
2022-11-11 12:25:36 -0500

    unused contexts

--- screens/Grades.tsx
+++ screens/Grades.tsx
@@ -1,35 +1,28 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import CourseDetails from './CourseDetails'
 import Courses from './Courses'
-import GradeContext from '../contexts/GradeContext'
 
 const Stack = createNativeStackNavigator()
 
 const StackNavigator = () => {
-  const [course, setCourse] = useState(undefined as string)
-
   return (
-    <GradeContext.Provider
-      value={{ courseHeader: course, setCourse: setCourse }}
-    >
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Courses"
-          component={Courses}
-          options={{
-            headerShown: false
-          }}
-        />
-        <Stack.Screen
-          name="Course Details"
-          component={CourseDetails}
-          options={{
-            headerShown: false
-          }}
-        />
-      </Stack.Navigator>
-    </GradeContext.Provider>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Courses"
+        component={Courses}
+        options={{
+          headerShown: false
+        }}
+      />
+      <Stack.Screen
+        name="Course Details"
+        component={CourseDetails}
+        options={{
+          headerShown: false
+        }}
+      />
+    </Stack.Navigator>
   )
 }
 
