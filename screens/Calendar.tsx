46b253a8a80375ddec18ff7d338962b1a855db85
VapoR <ryvapor@gmail.com>
2022-11-15 02:52:46 -0500

    get all calendar items with one request

--- screens/Calendar.tsx
+++ screens/Calendar.tsx
@@ -1,26 +1,27 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { StyleSheet, View, Text, BackHandler } from 'react-native'
+import React, { useContext, useState } from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+  BackHandler,
+  ActivityIndicator
+} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import AppContext from '../contexts/AppContext'
-import { Calendar } from 'studentvue'
-import { Agenda, AgendaEntry, DateData } from 'react-native-calendars'
+
+import { Agenda, AgendaEntry, AgendaSchedule } from 'react-native-calendars'
 import useAsyncEffect from 'use-async-effect'
 import { prependZero } from '../gradebook/GradeUtil'
 import Item from '../components/Item'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
 
 const CalendarScreen = ({ navigation }) => {
   const { client } = useContext(AppContext)
-  const [calendar, setCalendar] = useState(undefined as Calendar)
-  const [items, setItems] = useState({})
+  const [items, setItems] = useState(undefined as AgendaSchedule)
   const [startDate, setStartDate] = useState(undefined as string)
   const [endDate, setEndDate] = useState(undefined as string)
-  const [monthsLoaded, setMonthsLoaded] = useState([] as string[])
 
   useAsyncEffect(async () => {
-    try {
-      setCalendar(await client.calendar())
-    } catch (err) {}
-
     const backAction = () => {
       navigation.goBack()
       return true
@@ -31,64 +32,59 @@ const CalendarScreen = ({ navigation }) => {
       backAction
     )
 
+    try {
+      const calendar = await client.calendar()
+
+      const start =
+        calendar.schoolDate.start instanceof Date
+          ? calendar.schoolDate.start
+          : new Date(calendar.schoolDate.start)
+      setStartDate(
+        `${start.getFullYear()}-${prependZero(
+          start.getMonth() + 1
+        )}-${start.getDate()}`
+      )
+      const end =
+        calendar.schoolDate.end instanceof Date
+          ? calendar.schoolDate.end
+          : new Date(calendar.schoolDate.end)
+      setEndDate(
+        `${end.getFullYear()}-${prependZero(
+          end.getMonth() + 1
+        )}-${end.getDate()}`
+      )
+
+      const fullCalendar = await client.calendar({
+        interval: {
+          start: start,
+          end: end
+        }
+      })
+
+      const currentItems: AgendaSchedule = {}
+      for (const date of getDatesFromDateRange(start, end)) {
+        currentItems[toTimeString(date)] = []
+      }
+      for (const event of fullCalendar.events) {
+        const dateString = toTimeString(event.date)
+        currentItems[dateString] = []
+        currentItems[dateString].push({
+          name: `${event.startTime && `${event.startTime} - `}${event.title}`,
+          height: 30,
+          day: dateString
+        })
+      }
+      const newItems = {}
+      Object.keys(currentItems).forEach((key) => {
+        newItems[key] = currentItems[key]
+      })
+      setItems(newItems)
+    } catch (err) {}
+
     return () => backHandler.remove()
   }, [])
 
-  useEffect(() => {
-    if (calendar) {
-      const start = calendar.schoolDate.start
-      if (start instanceof Date) {
-        setStartDate(
-          `${start.getFullYear()}-${prependZero(
-            start.getMonth() + 1
-          )}-${start.getDate()}`
-        )
-      }
-      const end = calendar.schoolDate.end
-      if (end instanceof Date) {
-        setEndDate(
-          `${end.getFullYear()}-${prependZero(
-            end.getMonth() + 1
-          )}-${end.getDate()}`
-        )
-      }
-    }
-  }, [calendar])
-
-  const loadItems = async (day: DateData) => {
-    const months: string[] = monthsLoaded
-    if (months.some((m) => m === `${day.month} ${day.year}`)) {
-      return
-    }
-    const startOfMonth = new Date(day.year, day.month, 1)
-    const endOfMonth = new Date(day.year, day.month + 1, 0)
-    const currentItems: {} = items
-    for (const date of getDatesFromDateRange(startOfMonth, endOfMonth)) {
-      currentItems[date.toISOString().split('T')[0]] = []
-    }
-    const monthCalendar = await client.calendar({
-      interval: {
-        start: startOfMonth,
-        end: endOfMonth
-      }
-    })
-    for (const event of monthCalendar.events) {
-      const dateString = event.date.toISOString().split('T')[0]
-      currentItems[dateString] = []
-      currentItems[dateString].push({
-        name: `${event.startTime && `${event.startTime} - `}${event.title}`
-      })
-    }
-    const newItems = {}
-    Object.keys(currentItems).forEach((key) => {
-      newItems[key] = currentItems[key]
-    })
-    setItems(newItems)
-    months.push(`${day.month} ${day.year}`)
-    setMonthsLoaded(months)
-  }
-
-  function getDatesFromDateRange(from: Date, to: Date): Date[] {
+  const getDatesFromDateRange = (from: Date, to: Date): Date[] => {
     const dates = []
     for (let date = from; date < to; date.setDate(date.getDate() + 1)) {
       const cloned = new Date(date.valueOf())
@@ -98,9 +94,14 @@ const CalendarScreen = ({ navigation }) => {
     return dates
   }
 
-  function renderItem(item: AgendaEntry) {
+  const renderItem = (item: AgendaEntry) => {
     return <Item item={item}></Item>
   }
+
+  const toTimeString = (date: Date) => {
+    return date.toISOString().split('T')[0]
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }} edges={['top', 'left', 'right']}>
       <View style={styles.row_container}>
@@ -108,14 +109,26 @@ const CalendarScreen = ({ navigation }) => {
           <Text style={styles.title}>Calendar</Text>
         </View>
       </View>
-      <Agenda
-        items={items}
-        loadItemsForMonth={(data) => loadItems(data)}
-        renderItem={(item) => renderItem(item)}
-        minDate={startDate}
-        maxDate={endDate}
-        removeClippedSubviews
-      />
+      {startDate && endDate && items ? (
+        <Agenda
+          items={items}
+          renderItem={(item) => renderItem(item)}
+          minDate={startDate}
+          maxDate={endDate}
+          removeClippedSubviews
+        />
+      ) : (
+        <ActivityIndicator
+          color={Colors.secondary}
+          animating={true}
+          size="large"
+          style={{
+            alignSelf: 'center',
+            flex: 1,
+            justifyContent: 'center'
+          }}
+        />
+      )}
     </SafeAreaView>
   )
 }
