94387b5e2efddb2a143c814530708a6d0b2d07f4
VapoR <ryvapor@gmail.com>
2022-12-22 13:09:06 -0500

    unused props

--- components/Course.tsx
+++ components/Course.tsx
@@ -1,12 +1,5 @@
 import React from 'react'
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-  ViewStyle,
-  StyleProp
-} from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 import { Colors } from '../colors/Colors'
 import {
   calculateLetterGrade,
