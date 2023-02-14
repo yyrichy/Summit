2f538f641d8877c106b820c9c6dc46d8a784b645
VapoR <ryvapor@gmail.com>
2023-02-14 18:42:57 -0500

    1.1.8 22
    small fixes

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: 'GradeHelper',
     slug: 'Summit',
-    version: '1.1.7',
+    version: '1.1.8',
     orientation: 'portrait',
     icon: './assets/icon.png',
     platforms: ['android', 'ios'],
@@ -29,7 +29,7 @@ export default {
       },
       package: 'com.vaporys.Summit',
       softwareKeyboardLayoutMode: 'pan',
-      versionCode: 21
+      versionCode: 22
     },
     androidStatusBar: {
       barStyle: 'dark-content'
