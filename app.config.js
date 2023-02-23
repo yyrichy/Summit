d37ba7716bf63f15595a1e709ecbe640b842c6f7
VapoR <ryvapor@gmail.com>
2023-02-22 20:45:28 -0500

    1.2.5 28

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: 'GradeHelper',
     slug: 'Summit',
-    version: '1.2.4',
+    version: '1.2.5',
     orientation: 'portrait',
     icon: './assets/icon.png',
     platforms: ['android', 'ios'],
@@ -31,7 +31,7 @@ export default {
       },
       package: 'com.vaporys.Summit',
       softwareKeyboardLayoutMode: 'pan',
-      versionCode: 27
+      versionCode: 28
     },
     plugins: [
       [
