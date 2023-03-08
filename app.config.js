18a540d51d9817dc0eb58bbb1afa0f5fd98ecce2
VapoR <74574116+vaporrrr@users.noreply.github.com>
2023-03-08 13:15:00 -0500

    hermes

--- app.config.js
+++ app.config.js
@@ -6,7 +6,6 @@ export default {
     orientation: 'portrait',
     icon: './assets/icon.png',
     platforms: ['android', 'ios'],
-    jsEngine: 'jsc',
     splash: {
       image: './assets/splash.png',
       resizeMode: 'contain',
