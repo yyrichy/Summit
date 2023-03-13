bc988976ca874e6b9376cbfb5e6ad6786fdf0061
VapoR <74574116+vaporrrr@users.noreply.github.com>
2023-03-13 09:28:56 -0400

    1.3.8 38

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: 'GradeHelper',
     slug: 'Summit',
-    version: '1.3.7',
+    version: '1.3.8',
     orientation: 'portrait',
     icon: './assets/icon.png',
     platforms: ['android', 'ios'],
@@ -31,7 +31,7 @@ export default {
       },
       package: 'com.vaporys.Summit',
       softwareKeyboardLayoutMode: 'pan',
-      versionCode: 37
+      versionCode: 38
     },
     plugins: [
       [
